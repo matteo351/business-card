0f040215a4e0cbc1941461c0913f84ec43745486
matteo351 <matteo@kapa.ai>
2025-11-08 09:18:40 +0100

    updates

--- config/profile.js
+++ config/profile.js
@@ -3,12 +3,11 @@ export const artOrder = [ "avatar", "banner" ];
 export const profile = {
 	name: "Matteo Bulleri",
 	handle: "matteobulleri",
-	bio: [ "Matteo works as a commercial associate",
-		"Responsible for finding customers who desperately want a helpful AI assistant for their docs", 
+	bio: [ "Matteo is responsible for finding customers who desperately want a helpful AI assistant for their docs. ",
+		"When he doesn't do that, he does improv comedy, lifts weight, and reads non-fiction. ", 
 		"https://kapa.ai"],
 	infoSections: [
 		{ label: "Work", value: [ "Commercial Associate", "https://kapa.ai" ], style: "gradient", leadingNewline: true },
-		{ label: "Handle", value: "matteobulleri", style: "purple" },
 		{ label: "Email", value: "matteo@kapa.ai", style: "purple" },
 		{ label: "LinkedIn", value: "https://www.linkedin.com/in/matteo-bulleri/", style: "purple" },
 		{ label: "Card", value: "npx matteo-bulleri", style: "orange", leadingNewline: true }
