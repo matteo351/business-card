0fdcb77836a3a0a86b70d3b2ad250e51a2158bb9
matteo351 <matteo@kapa.ai>
2025-11-08 09:21:49 +0100

    upd

--- config/profile.js
+++ config/profile.js
@@ -4,8 +4,8 @@ export const profile = {
 	name: "Matteo Bulleri",
 	handle: "matteobulleri",
 	bio: [ "Matteo is responsible for finding customers who desperately want a helpful AI assistant for their docs. ",
-		"When he doesn't do that, he does improv comedy, lifts weight, and reads non-fiction. ", 
-		"https://kapa.ai"],
+		"When he doesn't do that, he does improv comedy, lifts some weight, and reads non-fiction. ",
+		"Matteo identifies as a curious boy who wants to learn new stuff."],
 	infoSections: [
 		{ label: "Work", value: [ "Commercial Associate", "https://kapa.ai" ], style: "gradient", leadingNewline: true },
 		{ label: "Email", value: "matteo@kapa.ai", style: "purple" },
